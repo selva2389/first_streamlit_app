cdd15f271998a2033dec5591af9322ac518bee9c
Selvalingam G <selvalingam29@gmail.com>
2023-08-10 20:51:30 +0530

    Update streamlit_app.py
    fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,10 +35,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+streamlit.text("The Fruit list contains:")
 streamlit.text(my_data_row)
+
+my_cnx = snowflake.connector.connect(
 
 
 # write your own comment -what does the next line do? 
